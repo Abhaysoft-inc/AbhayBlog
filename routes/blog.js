e094ecea585d7774b565155cb994adc06a14bdd6
Abhay Vishwakarma <57512017+Abhaysoft-inc@users.noreply.github.com>
2023-10-01 14:02:14 +0530

    Create blog.js

---
+++ routes/blog.js
@@ -0,0 +1,69 @@
+// routes/blog.js
+
+const express = require('express');
+const router = express.Router();
+const Blog = require('../models/blog');
+
+// Create a new blog post
+router.post('/', async (req, res) => {
+  try {
+    const blog = new Blog(req.body);
+    await blog.save();
+    res.status(201).json(blog);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get all blog posts
+router.get('/', async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.json(blogs);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Get a single blog post by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ error: 'Blog not found' });
+    }
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Update a blog post by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!blog) {
+      return res.status(404).json({ error: 'Blog not found' });
+    }
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Delete a blog post by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndRemove(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ error: 'Blog not found' });
+    }
+    res.json({ message: 'Blog deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
